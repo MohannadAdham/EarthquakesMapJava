71bba5afbc92507df471e9f9598d3948078bf608
Mohannad Adham <mohannad.adm@gmail.com>
2020-11-29 22:08:03 +0100

    Module 4 - Implemented the method isLand of the class EarthquakeCityMap.

--- UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
+++ UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
@@ -170,7 +170,9 @@ public class EarthquakeCityMap extends PApplet {
 		// If isInCountry ever returns true, isLand should return true.
 		for (Marker m : countryMarkers) {
 			// TODO: Finish this method using the helper method isInCountry
-			
+			if (isInCountry(earthquake, m)) {
+				return true;
+			}
 		}
 		
 		
