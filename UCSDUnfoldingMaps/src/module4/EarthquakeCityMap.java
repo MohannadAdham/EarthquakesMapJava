3a7e0f828aff6c8731926436a4299f414ed11104
Mohannad Adham <mohannad.adm@gmail.com>
2020-11-30 18:02:39 +0100

    Module 4 - Added the keys of the Ocean Quake and the Size of quakes to the legend.

--- UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
+++ UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
@@ -162,6 +162,15 @@ public class EarthquakeCityMap extends PApplet {
 		fill(color(0, 0, 0));
 		text("Land Quake", 75, 125);
 		
+		// Add the OceanQuake key
+		fill(color(255, 255, 255));
+		rect(45, 145, 10, 10);
+		fill(color(0, 0, 0));
+		text("Ocean Quake", 75, 150);
+		
+		// Add the size key
+		fill(color(0, 0, 0));
+		text("Size ~ Magnitude", 45, 170);
 	}
 
 	
